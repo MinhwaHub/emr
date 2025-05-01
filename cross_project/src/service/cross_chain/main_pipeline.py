b6bd78521c39a9673c4e2338059f2eab658524e3
minhwahub <somethingthatbelongstome@gmail.com>
2025-05-01 22:43:24 +0900

    add dl_token to main pipeline

--- cross_project/src/service/cross_chain/main_pipeline.py
+++ cross_project/src/service/cross_chain/main_pipeline.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from src.service.cross_wallet.dl import dl_cross_wallet
 from src.service.cross_wallet.dm import dm_token_price_d
 
-from src.service.cross_chain.dl import dl_cross_chain
+from src.service.cross_chain.dl import dl_cross_chain, dl_token
 from src.service.cross_chain.dw import dw_bridge_details, dw_cross_stats_d
 from src.service.cross_chain.dm import dm_bridge_d
 from src.utils.spark_utils import run_pipeline
@@ -22,6 +22,7 @@ STEPS = [
     ("dl_cross_chain", dl_cross_chain.main),
     ("dl_cross_wallet", dl_cross_wallet.main),
     ("dm_token_price_d", dm_token_price_d.main),
+    ("dl_token", dl_token.main),
     # wallet batch
     ("dw_cross_stats_d", dw_cross_stats_d.main),
     ("dw_bridge_details", dw_bridge_details.main),
